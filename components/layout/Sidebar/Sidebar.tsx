8967ea2215fcbd42c20d4c120339cc01d1fff0b6
saitogo555 <sg.saitogo@gmail.com>
2025-02-19 01:31:48 +0900

    fix: 閉じた状態でborderが二重になるバグを修正
    - アニメーションクラスの付与を子要素から親要素に変更

--- components/layout/Sidebar/Sidebar.tsx
+++ components/layout/Sidebar/Sidebar.tsx
@@ -31,13 +31,16 @@ export const Sidebar = () => {
 	}, [handleKeydown]);
 
 	return (
-		<div className="relative z-20 border-theme-border border-r">
+		<div
+			className={cn("relative z-20 border-theme-border border-r", "transition-all duration-300", {
+				"-translate-x-full -mr-(--sidebar-width)": !sidebar.isOpen,
+			})}
+		>
 			<div
 				className={cn(
 					"flex h-full w-(--sidebar-width) select-none flex-col bg-theme-primary py-1",
-					"transition-all duration-300",
+
 					"max-medium:absolute max-medium:top-0 max-medium:z-20",
-					{ "-translate-x-full -mr-(--sidebar-width)": !sidebar.isOpen },
 				)}
 			>
 				<h2 className={cn("py-2 pl-6 text-theme-text-primary text-xs", "max-medium:text-base")}>
