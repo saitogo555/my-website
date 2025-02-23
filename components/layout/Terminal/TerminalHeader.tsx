678964d160e97adf6e382395ceb1184bc82f9332
saitogo555 <sg.saitogo@gmail.com>
2025-02-23 23:54:46 +0900

    feat: ターミナルコンポーネントの追加
    - ターミナルの表示・非表示のトグル機能のみ実装

--- components/layout/Terminal/TerminalHeader.tsx
+++ components/layout/Terminal/TerminalHeader.tsx
@@ -1,3 +1,10 @@
+import { cn } from "@/utils";
+import { VscClose } from "react-icons/vsc";
+
+type Props = {
+  onClose: () => void
+}
+
 const items: string[] = [
 	"問題",
 	"出力",
@@ -9,14 +16,27 @@ const items: string[] = [
 	"コメント",
 ];
 
-export const TerminalHeader = () => {
+export const TerminalHeader = ({ onClose }: Props) => {
 	return (
-		<div>
-			<ul>
+		<div className="flex items-center justify-between px-6 py-1">
+			<ul className="flex items-center gap-6">
 				{items.map((item) => (
-					<li key={item}>{item}</li>
+					<li
+						className={cn("cursor-pointer text-primary-text/60 text-xs", "hover:text-primary-text")}
+						key={item}
+					>
+						{item}
+					</li>
 				))}
 			</ul>
+			<button
+				className={cn("cursor-pointer rounded p-1 text-primary-text transition", "hover:bg-neutral-300/10")}
+				type="button"
+				aria-label="ターミナルを閉じる"
+        onClick={onClose}
+			>
+				<VscClose className="text-primary-text" />
+			</button>
 		</div>
 	);
 };
