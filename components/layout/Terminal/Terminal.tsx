731a4da004cfcb918a53a7b0d57d71d8d19ae1d7
saitogo555 <sg.saitogo@gmail.com>
2025-02-23 23:54:46 +0900

    feat: ターミナルコンポーネントの追加
    - ターミナルの表示・非表示のトグル機能のみ実装

--- components/layout/Terminal/Terminal.tsx
+++ components/layout/Terminal/Terminal.tsx
@@ -1,9 +1,35 @@
+"use client";
+
+import { useCallback, useEffect, useState } from "react";
 import { TerminalHeader } from "./TerminalHeader";
+import { cn } from "@/utils";
 
 export const Terminal = () => {
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+
+	const toggleTerminal = useCallback(() => setIsOpen((prev) => !prev), []);
+
+	const closeTerminal = () => setIsOpen(false);
+
+	const handleKeydown = useCallback(
+		(e: KeyboardEvent) => {
+			if (!(e.ctrlKey && e.key === "@")) return
+			e.preventDefault()
+			toggleTerminal();
+		},
+		[toggleTerminal],
+	);
+
+	useEffect(() => {
+		window.addEventListener("keydown", handleKeydown);
+		return () => {
+			window.removeEventListener("keydown", handleKeydown);
+		};
+	}, [handleKeydown]);
+
 	return (
-		<div className="bg-primary border-t border-t-divider">
-			<TerminalHeader />
+		<div className={cn(" bg-primary", isOpen ? "h-96 border-t border-t-divider" : "h-0")}>
+			<TerminalHeader onClose={closeTerminal} />
 		</div>
 	);
 };
--- components/layout/Terminal/TerminalHeader.tsx
+++ components/layout/Terminal/TerminalHeader.tsx
@@ -1,22 +1,42 @@
-const items: string[] = [
-  "問題",
-  "出力",
-  "デバッグコンソール",
-  "ターミナル",
-  "ポート",
-  "SPELL CHECKER",
-  "GITLENS",
-  "コメント",
-]
+import { cn } from "@/utils";
+import { VscClose } from "react-icons/vsc";
 
-export const TerminalHeader = () => {
-  return (
-    <div>
-      <ul>
-        {items.map(item => (
-          <li key={item}>{item}</li>
-        ))}
-      </ul>
-    </div>
-  )
+type Props = {
+  onClose: () => void
 }
+
+const items: string[] = [
+	"問題",
+	"出力",
+	"デバッグコンソール",
+	"ターミナル",
+	"ポート",
+	"SPELL CHECKER",
+	"GITLENS",
+	"コメント",
+];
+
+export const TerminalHeader = ({ onClose }: Props) => {
+	return (
+		<div className="flex items-center justify-between px-6 py-1">
+			<ul className="flex items-center gap-6">
+				{items.map((item) => (
+					<li
+						className={cn("cursor-pointer text-primary-text/60 text-xs", "hover:text-primary-text")}
+						key={item}
+					>
+						{item}
+					</li>
+				))}
+			</ul>
+			<button
+				className={cn("cursor-pointer rounded p-1 text-primary-text transition", "hover:bg-neutral-300/10")}
+				type="button"
+				aria-label="ターミナルを閉じる"
+        onClick={onClose}
+			>
+				<VscClose className="text-primary-text" />
+			</button>
+		</div>
+	);
+};
