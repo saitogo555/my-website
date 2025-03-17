2ec1a595e930bb34568c177883753dc26bac2e98
saitogo555 <sg.saitogo@gmail.com>
2025-03-18 05:04:31 +0900

    style: TechnologyChipコンポーネントのフォントサイズとアイコンサイズを調整

--- features/works/components/TechnologyChip/TechnologyChip.tsx
+++ features/works/components/TechnologyChip/TechnologyChip.tsx
@@ -9,10 +9,10 @@ type Props = {
 
 export const TechnologyChip = ({ name, iconSrc, version }: Props) => {
 	return (
-		<Chip className="flex items-center">
-			<Image src={iconSrc} alt={name} className="mr-2 h-4" />
+		<Chip className="flex items-center text-base">
+			<Image src={iconSrc} alt={name} className="mr-2 h-5" />
 			<span>{name}</span>
-			{version && <span className="ml-1">{version}</span>}
+			{version && <span className="ml-1 text-gray-500">{version}</span>}
 		</Chip>
 	);
 };
