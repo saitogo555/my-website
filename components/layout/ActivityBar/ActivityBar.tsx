3184b7b520dc46dc9c05c634e5758f8a5a1c240d
saitogo555 <sg.saitogo@gmail.com>
2025-03-30 22:46:13 +0900

    feat(ActivityBar): QRコード表示機能を追加

--- components/layout/ActivityBar/ActivityBar.tsx
+++ components/layout/ActivityBar/ActivityBar.tsx
@@ -2,7 +2,7 @@
 
 import { useSidebar } from "@/hooks/useSidebar";
 import { cn } from "@/utils";
-import { type ReactNode, memo } from "react";
+import { type ReactNode, memo, useState } from "react";
 import {
 	VscAccount,
 	VscExtensions,
@@ -14,6 +14,7 @@ import {
 import { ActivityButton } from "./ActivityButton";
 import { ActivityLink } from "./ActivityLink";
 import { MdQrCodeScanner } from "react-icons/md";
+import { QRCode } from "../QRCode";
 
 type ActivityLinkType = {
 	to: string;
@@ -30,30 +31,43 @@ const items: ActivityLinkType[] = [
 ];
 
 export const ActivityBar = memo(() => {
+	const [isQrCodeVisible, setQrCodeVisible] = useState<boolean>(false);
 	const sidebar = useSidebar();
 
+	const handleClickQrCodeOpenButton = () => {
+		setQrCodeVisible(true);
+	};
+
+	const handleClickQrCodeCloseButton = () => {
+		setQrCodeVisible(false);
+	};
+
 	return (
-		<aside
-			className={cn(
-				"relative z-30 flex h-full w-(--activity-bar-width) shrink-0 flex-col",
-				"overflow-y-auto overflow-x-hidden border-divider border-r bg-primary",
-			)}
-		>
-			<ActivityButton
-				icon={<VscFiles />}
-				title="ナビゲーション"
-				aria-label={sidebar.isOpen ? "メニューを閉じる" : "メニューを開く"}
-				onClick={() => sidebar.toggle()}
-			/>
-			{items.map((item) => (
-				<ActivityLink to={item.to} title={item.title} icon={item.icon} key={item.to.toString()} />
-			))}
-			<ActivityButton
-				className="mt-auto"
-				icon={<MdQrCodeScanner />}
-				title="QRコード"
-				aria-label="QRコードを表示"
-			/>
-		</aside>
+		<>
+			<aside
+				className={cn(
+					"relative z-30 flex h-full w-(--activity-bar-width) shrink-0 flex-col",
+					"overflow-y-auto overflow-x-hidden border-divider border-r bg-primary",
+				)}
+			>
+				<ActivityButton
+					icon={<VscFiles />}
+					title="ナビゲーション"
+					aria-label={sidebar.isOpen ? "メニューを閉じる" : "メニューを開く"}
+					onClick={() => sidebar.toggle()}
+				/>
+				{items.map((item) => (
+					<ActivityLink to={item.to} title={item.title} icon={item.icon} key={item.to.toString()} />
+				))}
+				<ActivityButton
+					className="mt-auto"
+					icon={<MdQrCodeScanner />}
+					title="QRコード"
+					aria-label="QRコードを表示"
+					onClick={handleClickQrCodeOpenButton}
+				/>
+			</aside>
+			{isQrCodeVisible && <QRCode onClose={handleClickQrCodeCloseButton} />}
+		</>
 	);
 });
