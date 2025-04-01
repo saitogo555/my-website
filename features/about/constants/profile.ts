5f41bcbf1fc25a5630fc955ea1e1ed97400f7387
saitogo555 <sg.saitogo@gmail.com>
2025-04-02 01:00:11 +0900

    fix(profile): 年齢計算のロジックを簡素化
    - calcAge関数を削除し、直接ageを計算するように変更

--- features/about/constants/profile.ts
+++ features/about/constants/profile.ts
@@ -1,23 +1,18 @@
 const birthYear = 2001;
 const birthMonth = 5;
 const birthDate = 16;
-const calcAge = () => {
-	const today = new Date();
-	const birthdate = new Date(birthYear, birthMonth - 1, birthDate);
-	const val = today.getFullYear() - birthdate.getFullYear();
-	const monthDiff = today.getMonth() - birthdate.getMonth();
-	const dayDiff = today.getDate() - birthdate.getDate();
-	// 今年の誕生日がまだ来ていない場合、年齢を1つ減らす
-	if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
-		return val - 1;
-	}
-	return val;
-};
+const today = new Date();
+const birthdate = new Date(birthYear, birthMonth - 1, birthDate);
+const yearDiff = today.getFullYear() - birthdate.getFullYear();
+const monthDiff = today.getMonth() - birthdate.getMonth();
+const dayDiff = today.getDate() - birthdate.getDate();
+// 今年の誕生日がまだ来ていない場合、年齢を1つ減らす
+const age = monthDiff < 0 || (monthDiff === 0 && dayDiff < 0) ? yearDiff - 1 : yearDiff;
 
 export const PROFILE = {
 	name: "齋藤 豪",
 	ruby: "サイトウ ゴウ",
-	age: calcAge(),
+	age: age,
 	birthday: `${birthYear}/${birthMonth}/${birthDate}`,
 	bloodType: "A(+)",
 	birthPlace: "愛知県 名古屋市",
