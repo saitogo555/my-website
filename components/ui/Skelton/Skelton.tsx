ec2579df852a1fc3bc823545a7599f771fedafc5
saitogo555 <sg.saitogo@gmail.com>
2025-04-03 01:02:52 +0900

    feat(Skelton): HTML属性を受け取れるように変更
    - className以外のHTML属性をSkeltonコンポーネントに渡せるようにした

--- components/ui/Skelton/Skelton.tsx
+++ components/ui/Skelton/Skelton.tsx
@@ -1,12 +1,13 @@
 import { cn } from "@/utils";
+import type { HTMLAttributes } from "react";
 
 type Props = {
 	className?: string;
-};
+} & Omit<HTMLAttributes<HTMLDivElement>, "className">;
 
-export const Skelton = ({ className }: Props) => {
+export const Skelton = ({ className, ...props }: Props) => {
 	return (
-		<div className={cn("animate-skelton overflow-hidden rounded-lg", className)}>
+		<div className={cn("animate-skelton overflow-hidden rounded-lg", className)} {...props}>
 			<div className="h-full w-full bg-secondary/50 dark:bg-primary/50" />
 		</div>
 	);
