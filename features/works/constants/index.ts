e37d0dba8973ea3d1b00b113da06161ec201581c
saitogo555 <sg.saitogo@gmail.com>
2025-04-22 03:05:10 +0900

    feat(works): Rink SAUNAの情報を追加
    - Rink SAUNAのデータを新規作成
    - WORK_LISTにRink SAUNAを追加

--- features/works/constants/index.ts
+++ features/works/constants/index.ts
@@ -1,5 +1,6 @@
+import { RINK_SAUNA } from "./rink-sauna";
 import { WELLSAVERS } from "./wellsavers";
 import { ZUIKO_FESTIVAL_2022 } from "./zuiko-festival-2022";
 import { ZUNKO } from "./zunko";
 
-export const WORK_LIST = [ZUIKO_FESTIVAL_2022, WELLSAVERS, ZUNKO] as const;
+export const WORK_LIST = [ZUIKO_FESTIVAL_2022, WELLSAVERS, ZUNKO, RINK_SAUNA] as const;
