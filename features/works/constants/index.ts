3cc8a9af2874f1b48967c6d9c08706d477c399f2
saitogo555 <sg.saitogo@gmail.com>
2025-06-09 21:34:56 +0900

    feat(works): Discord Readme Botの情報を追加
    - Discordサーバーでメッセージの既読管理とリマインド機能を提供するBotの詳細を追加
    - 画像とリンク情報を含む

--- features/works/constants/index.ts
+++ features/works/constants/index.ts
@@ -1,6 +1,7 @@
+import { DISCORD_README_BOT } from "./discord-readme-bot";
 import { RINK_SAUNA } from "./rink-sauna";
 import { WELLSAVERS } from "./wellsavers";
 import { ZUIKO_FESTIVAL_2022 } from "./zuiko-festival-2022";
 import { ZUNKO } from "./zunko";
 
-export const WORK_LIST = [ZUIKO_FESTIVAL_2022, WELLSAVERS, ZUNKO, RINK_SAUNA] as const;
+export const WORK_LIST = [ZUIKO_FESTIVAL_2022, WELLSAVERS, ZUNKO, RINK_SAUNA, DISCORD_README_BOT] as const;
